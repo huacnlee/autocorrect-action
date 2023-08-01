1101f11d290488f38b71b771e392424e987693b1
Jason Lee <huacnlee@gmail.com>
2023-08-01 21:05:55 +0800

    Update script

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@ set -e
 
 # If USE_NPM=true, use npm version of autocorrect
 if [ "$USE_NPM" = "true" ]; then
+    echo "Execute autocorrect-node ..."
     yarn autocorrect $*
 else
     /usr/local/bin/autocorrect $*
