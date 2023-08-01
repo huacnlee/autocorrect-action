6d08b060462dfe534f021d5f650903cbc54e2d75
Jason Lee <huacnlee@gmail.com>
2023-08-01 21:07:20 +0800

    Exec yarn before exec yarn autocorrect

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@ set -e
 # If USE_NPM=true, use npm version of autocorrect
 if [ "$USE_NPM" = "true" ]; then
     echo "Execute autocorrect-node ..."
+    yarn
     yarn autocorrect $*
 else
     /usr/local/bin/autocorrect $*
