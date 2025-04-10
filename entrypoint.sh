0d23676dba738aaa1f710396b2c7b93a5baa3ce5
chenhaoran <haoran920c@163.com>
2025-04-10 19:21:32 +0800

    Add target input to action.yml and enhance entrypoint.sh argument handling

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,6 @@ set -e
 
 bin=/usr/local/bin/autocorrect
 
-
-
 # If USE_NPM=true, use npm version of autocorrect
 if [ "$USE_NPM" = "true" ]; then
     echo "Use autocorrect-node"
@@ -13,7 +11,8 @@ fi
 
 if [ "$REVIEWDOG" = "true" ]; then
     echo "Use reviewdog"
-    $bin --lint . --format rdjson | reviewdog -f=rdjson -reporter=github-pr-review -level=warning
+    args=${ARGS:-". --lint --no-diff-bg-color"}
+    $bin $args --format rdjson | reviewdog -f=rdjson -reporter=github-pr-review -level=warning
 else
-    $bin $*
+    $bin "$@"
 fi
